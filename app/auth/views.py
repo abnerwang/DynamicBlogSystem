121c4d01d0364f8bf231e417a035ac228d146693
abnerwang <Wxp@0910>
2018-02-01 17:35:45 +0800

    用户登录登出模块完成

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, flash
+from flask import render_template, flash, redirect, request, url_for
+from flask_login import login_user, logout_user, login_required
 
 from . import auth
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserLoginForm
 from .. import db
 from ..models import User
 
@@ -14,5 +15,25 @@ def register():
         db.session.add(user)
 
         flash('注册成功，你现在可以登录了！')
-
+        redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = UserLoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email_address=form.email.data).first()
+        if user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('密码错误！')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    flash('您已成功登出！')
+    return redirect(url_for('main.index'))
