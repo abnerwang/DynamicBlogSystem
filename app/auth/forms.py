9452e144a0ac091a9ebbc03ca1a18c89db4c968e
abnerwang <Wxp@0910>
2018-02-01 19:14:35 +0800

    用户登录登出模块完成

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, ValidationError
+from wtforms import StringField, SubmitField, ValidationError, BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, Regexp
 
 from ..models import User
@@ -8,7 +8,7 @@ from ..models import User
 class UserRegisterForm(FlaskForm):
     student_id = StringField('学号', validators=[DataRequired(), Length(min=9, max=10, message='学号格式不正确！')])
     username = StringField('用户名', validators=[DataRequired(), Length(min=1, max=64, message='用户名长度为 1 到 64 位！')])
-    email = StringField('电子邮件地址', validators=[DataRequired(), Email()])
+    email = StringField('电子邮件地址', validators=[DataRequired(), Email(message='必须是有效的 Email 地址！')])
     password = StringField('密码', validators=[DataRequired(), EqualTo('password2', message='两次输入的密码不一致！'),
                                              Regexp('^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$',
                                                     message='密码必须同时含有字母、数字和特殊字符，且不能低于 8 位！')])
@@ -26,3 +26,15 @@ class UserRegisterForm(FlaskForm):
     def validate_email(self, field):
         if User.query.filter_by(email_address=field.data).first():
             raise ValidationError('该电子邮件已注册！')
+
+
+class UserLoginForm(FlaskForm):
+    email = StringField('电子邮件地址', validators=[DataRequired(), Email(message='必须是有效的 Email 地址！')])
+    password = StringField('密码', validators=[DataRequired(message='密码不能为空！')])
+    remember_me = BooleanField('记住我')
+    submit = SubmitField('登录')
+
+    def validate_email(self, field):
+        user = User.query.filter_by(email_address=field.data).first()
+        if user is None:
+            raise ValidationError('您所输入的电子邮件地址尚未注册！')
