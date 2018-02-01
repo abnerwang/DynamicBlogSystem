4b232300a81de6ccbfe3cb25511fb567411995d4
abnerwang <Wxp@0910>
2018-02-01 19:19:20 +0800

    用户登录登出模块完成

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db
+from . import login_manager
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(64), unique=True)
@@ -21,3 +23,7 @@ class User(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
