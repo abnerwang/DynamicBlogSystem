48e7cce87b6c802983c72ce37930780be7007c58
abnerwang <Wxp@0910>
2018-02-05 10:47:48 +0800

    修改部署相关问题

--- DynamicBlogSystem.py
+++ DynamicBlogSystem.py
@@ -2,7 +2,7 @@ import os
 
 import pymysql
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Server
 
 from app import create_app, db
 from app.models import User
@@ -19,6 +19,7 @@ def make_shell_context():
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server(host='0.0.0.0', port=80))
 
 if __name__ == '__main__':
     manager.run()
